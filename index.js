c0a1ba9d6679f7142d86c1331bbae509a94d8f80
sara ahmed <sara.bakr.ahmad@gmail.com>
2022-11-02 23:44:07 +0300

    commited

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+
+
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+
+    if (left < 400) {
+      dodger.style.left = `${left + 1}px`;
+    }
+}
+
+const the_thing=document.getElementById('dodger');
+function movingRight(){
+    
+    the_thing.addEventListener('click',moveDodgerRight);
+}
+movingRight();
+
+
